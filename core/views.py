b6e6d2bdede1c3f5a36228b0243c67e91c30217a
Rafael Gauna Trindade <rtrindade@inf.ufsm.br>
2018-08-18 01:53:04 -0300

    Criação view do clima

--- core/views.py
+++ core/views.py
@@ -8,3 +8,8 @@ def index(request):
     consumoDia = [[i, randint(100, 200)] for i in range(20)]
     print(consumoDia)
     return render(request, 'core/index.html', {'consumoDia':consumoDia})
+
+@login_required
+def climate(request):
+    
+    return render(request, 'core/climate.html')
