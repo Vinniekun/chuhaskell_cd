4d595e93e79e8c3336f0ab8ccfcd18b03e83f9f2
Cassiano Andrei <cschneider@inf.ufsm.br>
2018-08-18 09:19:11 -0300

    ajustes

--- core/populator.py
+++ core/populator.py
@@ -1,17 +1,18 @@
 from sensor.models import Historico, Sensor
 import random
 from datetime import datetime, timedelta
+import time
 
+Historico.objects.all().delete()
 while True:
     ids = list(Sensor.objects.all().values("id").distinct())
     for dias in range(0,30):
         ago = datetime.now() - timedelta(days=dias)
         for i in ids:
-            print(i.values())
             temp = random.randrange(0, 40)
             umi = random.randrange(0,100)
             luz = random.randrange(0,100)
             if Sensor.objects.get(id=i['id']).status:   
                 h = Historico.objects.create(sensor_id=i['id'],temperatura=temp,umidade=umi,luz=luz,time=ago)
                 h.save()
-    sleep(10)
+    time.sleep(10)
